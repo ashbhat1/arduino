a0dd47de79dcb15f3816d11551e0d907fb1cbf01
Ashwini <ashwinibhat1@gmail.com>
2017-07-06 00:48:56 -0700

    adding blinky_c and some simple c/c++ programs taht will be added and documentation of serial_useful_links.txt

--- blinky/blinky_avr_script.sh
+++ blinky/blinky_avr_script.sh
@@ -1,4 +1,5 @@
-stty -F /dev/serial/by-id/usb-SparkFun_SparkFun_Pro_Micro-if00 1200;	
+#stty -F /dev/serial/by-id/usb-SparkFun_SparkFun_Pro_Micro-if00 1200;	
+stty -F /dev/ttyACM1 1200;
 sleep 1s;
 #don't need a close just need to start it
 #avrdude -C/home/ashwini/git-repos/arduino/avr-1.1.6/avrdude.conf -v -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D -Uflash:w:/tmp/arduino_build_264931/blinky.ino.hex:i
@@ -13,5 +14,5 @@ sleep 1s;
 
 #avrdude -C/home/ashwini/.arduino15/packages/SparkFun/hardware/avr/1.1.6/avrdude.conf -v -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D -Uflash:w:/home/ashwini/git-repos/arduino/blinky/blinky.ino.promicro.hex:i -v
 
-avrdude -C/home/ashwini/git-repos/arduino/avr-1.1.6/avrdude.conf -v -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D -Uflash:w:/home/ashwini/git-repos/arduino/blinky/blinky.ino.promicro.hex:i -v
+avrdude -C/home/ashwini/git-repos/arduino/avr-1.1.6/avrdude.conf -v -patmega32u4 -cavr109 -P/dev/ttyACM1 -b57600 -D -Uflash:w:/home/ashwini/git-repos/arduino/blinky/blinky.ino.promicro.hex:i -v
 
