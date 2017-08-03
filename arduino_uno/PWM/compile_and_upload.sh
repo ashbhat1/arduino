bd705b33cb5037a5aea9481ff91ddc711a0d1a03
Ashwini <ashwinibhat1@gmail.com>
2017-08-03 00:07:12 -0700

    added ability to use a PWM with timers, copied from spark fun pro micro with minor adjustments

---
+++ arduino_uno/PWM/compile_and_upload.sh
@@ -0,0 +1,7 @@
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o PWM.o PWM.c
+avr-gcc -mmcu=atmega328p PWM.o -o PWM
+avr-objcopy -O ihex -R .eeprom PWM PWM.hex
+
+#flashing the Arduino:
+#avrdude -V -F -C /etc/avrdude/avrdude.conf -p atmega328p -P /dev/ttyUSB0 -c stk500v1 -b 57600 -U flash:w:blink.hex
+avrdude -C/home/ashwini/Downloads/arduino-1.8.3/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:PWM.hex:i
